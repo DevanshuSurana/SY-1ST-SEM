d1a3cbe2eba99cfd79064ef41b0ef112b470708e
Devanshu Surana <104250225+DevanshuSurana@users.noreply.github.com>
2023-06-06 18:32:19 +0530

    SY 1ST SEM FILES

---
+++ PP/PP LAB /1/lab 1.py
@@ -0,0 +1,62 @@
+# first python file
+'''print("Studetn Name: Devanshu Surana")
+print("address: Pune")
+print("Contact-no:8999300859")
+print("Mother tongue: Hindi")
+print("School_Name:DYPIS")
+print("Year: 2022")
+print("Panel:c")
+print("roll_no:1032210755")'''
+
+
+
+
+
+
+
+
+#name = input("What is your name? \n")
+#print(name)
+
+
+'''print("hello")
+a=10;b=20
+c=a+b
+print(c)
+'''
+
+'''a= int(input("enter a value for a "))
+
+b= int(input("enter a value for b "))
+c=a+b
+print("addition of a with b is = ",c)
+c=a*b
+print("multiplication of a with b is = ",c)
+
+a= (input("enter a value for a"))
+
+b= 13
+(input("enter a value for b"))
+c=a+b
+print("addition of a with b is = ",c)
+c=a*b
+print("multiplication of a with b is = ",c)
+'''
+
+a= int(input("value a is "))
+b= int(input("value b is "))
+c= float(input("value c is "))
+d= float(input("value d is "))
+
+e=a+b
+f=a-b
+g=a*b
+h=a%b
+print("addition,substraction,multiplication,modulus",e,f,g,h)
+
+
+i=c+d
+j=c-d
+k=c*d
+l=c%d
+print("addition,substraction,multiplication,modulus",i,j,k,l)
